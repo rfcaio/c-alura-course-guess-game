9d893a5ac201489c7ffc16e52935293e86872850
Caio Rodrigues <rfcaio90@gmail.com>
2018-12-18 12:09:23 -0300

    c1-aula-03

--- guess.c
+++ guess.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main () {
-  int guess, secret = 47, shots = 1;
+  srand(time(0));
+  double score = 1000;
+  int guess, secret = rand() % 100, shots = 1;
 
   printf("\n\n***** WELCOME TO THE GUESS GAME!\n\n");
   while (1) {
@@ -16,6 +20,7 @@ int main () {
     if (guess == secret) {
       printf("\n***** BINGO! YOU HIT THE NUMBER.\n");
       printf("***** YOU DID IT IN %d %s.\n", shots, shots == 1 ? "SHOT" : "SHOTS");
+      printf("***** YOUR SCORE: %.2f.", score);
       break;
     } else {
       printf("***** WRONG! YOU DID NOT HIT THE NUMBER.\n");
@@ -24,6 +29,7 @@ int main () {
       } else {
         printf("***** YOUR NUMBER IS LOWER THAN THE SECRET.\n");
       }
+      score -= (double) abs(guess - secret) / 2;
       shots++;
     }
   }
