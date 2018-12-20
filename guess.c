1663a5687f72bdd99cf489642cdfd413e31221b5
Caio Rodrigues <rfcaio90@gmail.com>
2018-12-20 13:01:22 -0300

    c1-aula-04

--- guess.c
+++ guess.c
@@ -5,21 +5,44 @@
 int main () {
   srand(time(0));
   double score = 1000;
-  int guess, secret = rand() % 100, shots = 1;
+  int guess, level, secret = rand() % 100, shots;
 
   printf("\n\n***** WELCOME TO THE GUESS GAME!\n\n");
-  while (1) {
-    printf("\n***** TAKE A SHOT! ");
+  printf("(1) - EASY\n");
+  printf("(2) - MEDIUM\n");
+  printf("(3) - HARD\n");
+  printf("(4) - INSANE\n");
+  printf("\nCHOOSE YOUR DESTINY! ");
+  scanf("%d", &level);
+
+  switch (level) {
+    case 4:
+      shots = 1;
+      break;
+    case 3:
+      shots = 5;
+      break;
+    case 2:
+      shots = 10;
+      break;
+    default:
+      shots = 20;
+      break;
+  }
+
+  for (int i = 1; i <= shots; i++) {
+    printf("\n***** (%d) - TAKE A SHOT! ", i);
     scanf("%d", &guess);
 
     if (guess < 0) {
       printf("\n***** DO NOT SHOT A NEGATIVE NUMBER!\n");
+      i--;
       continue;
     }
 
     if (guess == secret) {
       printf("\n***** BINGO! YOU HIT THE NUMBER.\n");
-      printf("***** YOU DID IT IN %d %s.\n", shots, shots == 1 ? "SHOT" : "SHOTS");
+      printf("***** YOU DID IT IN %d %s.\n", i, i == 1 ? "SHOT" : "SHOTS");
       printf("***** YOUR SCORE: %.2f.", score);
       break;
     } else {
@@ -30,7 +53,6 @@ int main () {
         printf("***** YOUR NUMBER IS LOWER THAN THE SECRET.\n");
       }
       score -= (double) abs(guess - secret) / 2;
-      shots++;
     }
   }
   printf("\n***** THE GAME IS OVER.\n\n");
